ccd20f179768a72845f6a3b4bb8e16a4177e9c0b
carlos1999 <cvcsantos14@gmail.com>
2020-12-14 17:44:43 -0300

    jogo funcional primeira versão com duas fases

---
+++ Assets/Scripts/ControleNivel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ControleNivel : MonoBehaviour
+{
+    private Scene scene;
+    private bool paused;
+    public GameObject pauseMenu;
+    public GameObject controles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(paused){
+                Resume();
+                AudioListener.pause = false;
+            }else{
+                Pause();
+                AudioListener.pause = true;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && paused == false){
+            Time.timeScale = 0.5f;
+        }else if (paused == false){
+            Time.timeScale = 1f;
+        }
+    
+    }
+
+
+    void Resume(){
+        pauseMenu.SetActive(false);
+        controles.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+    public void Reiniciar(){
+        paused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(scene.name);
+    }
+     public void ProximaFase(){
+        paused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("fase 2");
+    }
+    public void VoltarFase(){
+        pauseMenu.SetActive(false);
+        paused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("fase 1");
+    }
+
+    public void Despausar(){
+        Resume();
+    }
+
+    public void MostrarControles(){
+        pauseMenu.SetActive(false);
+        controles.SetActive(true);
+    }
+
+    public void Voltar(){
+        controles.SetActive(false);
+        Pause();
+    }
+
+}
---
+++ Assets/Scripts/coletaveis.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class coletaveis : MonoBehaviour
+{
+    
+    public Text pontuacao;
+    private int valorPontuacao;
+    public GameObject sprite;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            valorPontuacao = Convert.ToInt32(pontuacao.text) +1;
+            pontuacao.text = ""+valorPontuacao; 
+            print("pegou um quadrado");
+            
+            sprite.SetActive(false);
+        }
+       
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
